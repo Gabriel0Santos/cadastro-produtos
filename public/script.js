b1dde3dafa650f8d3df2f894219357063bbdbceb
Gabriel Santos <you@example.com>
2025-04-15 01:07:35 -0300

    mk1

---
+++ public/script.js
@@ -0,0 +1,34 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+document.querySelector('.btn__salvar').addEventListener('click', async () => {
+    const produtoNome = document.querySelector('.produto__nome').value
+    const produtoPreco = parseFloat(document.querySelector('.produto__preco').value)
+    const produtoQuantidade = parseInt(document.querySelector('.produto__quantidade').value)
+
+    if (!produtoNome || isNaN(produtoPreco) || isNaN(produtoQuantidade) || produtoPreco <= 0 || produtoQuantidade <= 0 ) {
+        alert('Por favor, preencha todos os campos corretamente.')
+        return
+    }
+
+    const response = await fetch ('/cadastro', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+            produto: produtoNome,
+            preco: produtoPreco,
+            quantidade: produtoQuantidade
+        })
+
+    })
+
+    if (response.ok) {
+        alert('Produto salvo com sucesso!')
+    }
+    else {
+        alert('Erro ao salvar o produto.')
+    }
+})
+})
---
+++ server.js
@@ -0,0 +1,69 @@
+// importações
+import express from 'express'
+import mongoose, { mongo } from 'mongoose'
+import { fileURLToPath } from 'url'
+import path from 'path'
+import { error } from 'console'
+
+// Constantes
+const app = express()
+const PORT = 3000
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// Conexão ao Banco de Dados
+mongoose.connect('mongodb+srv://contareservaid01:RDwlGyUrOegrq1yq@clusterteste01.1qa4ysy.mongodb.net/?retryWrites=true&w=majority&appName=ClusterTeste01')
+    .then(() => console.log('Conectado ao MongoDB'))
+    .catch(err => console.error('Erro na conexão com o MongoDB', err));
+
+//modelo Mongoose
+
+const produto = mongoose.model('produto', {
+
+    produto: String,
+    preco: Number,
+    quantidade: Number
+
+})
+
+// Middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "view")));
+
+//POST
+app.post('/cadastro', async (req, res) => {
+    try{
+        if (!req.body.produto || !req.body.preco || !req.body.quantidade) {
+            return res.status(400).json({ message: 'Todos os campos são obrigatórios!' })
+        }
+
+        const ferramenta = new produto({
+            produto: req.body.produto,
+            preco: req.body.preco,
+            quantidade: req.body.quantidade
+        })
+        await ferramenta.save()
+        res.status(201).json({message: 'Produto cadastrado com sucesso'})
+    } catch (err) {
+        console.error('Erro ao salvar o produto:', err)
+        res.status(500).json({message: 'Erro ao salvar produto', error: err})
+    }
+
+
+})
+
+// GET
+
+app.get('/cadastro', (req,res) => {
+    res.sendFile(path.join(__dirname, "view", "index.html"))
+})
+
+
+// Chamada do Servidor
+
+app.listen( PORT, () => {
+    console.log(`O Servidor está funcinando bem na porta: localhost:${PORT}.`)
+})
+
+
